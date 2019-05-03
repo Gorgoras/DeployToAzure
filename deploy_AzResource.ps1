12691b92a6beff1cededa19bbb18a72d5727fd45
Martin Zurita <martinzurita1@gmail.com>
2019-05-03 12:53:57 -0300

    Update deploy_AzResource.ps1

--- deploy_AzResource.ps1
+++ deploy_AzResource.ps1
@@ -1,3 +1,3 @@
-﻿Connect-AzAccount
+﻿Connect-AzAccount #-Identity if its on a VM with a managed identity assigned, doesnt ask for credentials.
 
 New-AzResourceGroupDeployment -ResourceGroupName "HDINSIGHT-RG" -Location "eastus2" -TemplateParameterFile .\parameters.json -TemplateFile .\template.json
